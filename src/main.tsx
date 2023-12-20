006a5a0080b7a249f7841f6f55e9b27d14fc457c
yunamoon <tpwkao0051@gmail.com>
2023-12-20 13:52:50 +0900

    'yunamoon_1week'

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import  ReactDOM  from "react-dom/client";
+import App from "./app";
+
+
+function Demo({count} : {count:number}) {
+return ( <p> Demo {count}</p>)
+}
+
+function main() {
+const element = document.getElementById('root');
+const demoElement = document.getElementById('demo');
+
+if (!element || !demoElement) {
+    return;
+}
+
+const root =   ReactDOM.createRoot(element);
+const demoRoot = ReactDOM.createRoot(demoElement);
+let count = 0;
+
+root.render(<App/>);
+demoRoot.render(<Demo count={count}/>);
+
+
+setInterval(()=> {
+    count +=1;
+    demoRoot.render(<Demo count={count}/>);
+},1_000);
+
+}
+
+main();
