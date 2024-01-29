1fa470ba681bd75d2271e74ed34f6f84dddb2586
magrowing <kmagrowing@gmail.com>
2024-01-29 13:18:17 +0900

    add : main.test.tsx 파일  수정

---
+++ src/main.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+
+describe('index component', () => {
+	it('render root component.', () => {
+		const component = <App />;
+		const container = document.createElement('div');
+		container.setAttribute('id', 'root');
+		document.body.appendChild(container);
+
+		const root = createRoot(container);
+		root.render(component);
+
+		expect(document.body.firstChild).toEqual(container);
+	});
+});
